f2c36a90ac23ace392001733faa0011eff3db452
Thành Nguyễn <nguyennhuthanh0104@gmail.com>
2026-01-29 16:42:11 +0700

    prepare for deploy on Sevalla

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express"
 import {ENV} from "./lib/env.js";
+import * as path from "node:path";
 
 
 const app = express();
+
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json());
@@ -11,5 +14,14 @@ app.use(express.json());
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Api is up and running" });
 });
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 app.listen(ENV.PORT, () => console.log("Server is running on port", ENV.PORT));
